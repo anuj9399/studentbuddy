5b241105df747406ea5442460185ebc5635fabfb
Anuj <anujmhaisdhune@gmail.com>
2026-03-15 22:45:05 +0530

    Fix asgi.py AppRegistryNotReady error for Render deployment

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,24 +1,21 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Setup Django FIRST before any other imports
+django.setup()
+
+# Only import routing AFTER django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 import groups.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
+    'http': get_asgi_application(),
+    'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
                 groups.routing.websocket_urlpatterns
