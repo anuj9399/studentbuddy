2be2bd09fc80b0e3c834a28a290d2b5ade1acbfc
Anuj <anujmhaisdhune@gmail.com>
2026-03-15 22:55:00 +0530

    Fix SSL redirect loop on Render

--- core/core/settings.py
+++ core/core/settings.py
@@ -181,7 +181,7 @@ CHANNEL_LAYERS = {
 
 # Production Security Settings
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
